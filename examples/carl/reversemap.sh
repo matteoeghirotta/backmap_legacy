8d6dd6b4d98e23b4d97a6bc115320d4599a9eafa
Matteo Ricci <matteo.ricci@studiomado.it>
2021-01-04 19:54:32 +0100

    debug

--- examples/carl/reversemap.sh
+++ examples/carl/reversemap.sh
@@ -5,4 +5,4 @@ rm -f npb_substrate.0*
 ../../src/backmap -t npb/align1_bin.xyz -t npb/align2_naph.xyz -t npb/align3_phe.xyz \
 -o npb_substrate npb_substrate.dump
 
-lldb -- ../../src/backmap -t npb/align1_bin.xyz -t npb/align2_naph.xyz -t npb/align3_phe.xyz -o npb_substrate npb_substrate.dump
+# lldb -- ../../src/backmap -t npb/align1_bin.xyz -t npb/align2_naph.xyz -t npb/align3_phe.xyz -o npb_substrate npb_substrate.dump
